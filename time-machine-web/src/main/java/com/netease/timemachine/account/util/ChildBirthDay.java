10f874cb234611833502374246f761b7248961d3
wuqihua <15295556396@163.com>
2018-07-31 10:42:26 +0800

    @dev 孩子月数

--- time-machine-web/src/main/java/com/netease/timemachine/account/util/ChildBirthDay.java
+++ time-machine-web/src/main/java/com/netease/timemachine/account/util/ChildBirthDay.java
@@ -61,4 +61,29 @@ public class ChildBirthDay {
         }
         return tag.toString();
     }
+    public static Integer getChildMonths(Date date){
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        String str = df.format(date);
+        String[] data = str.split("-");
+        if (data.length < 3) {
+            return 0;
+        }
+        Calendar birthday = new GregorianCalendar(Integer.valueOf(data[0]), Integer.valueOf(data[1]), Integer.valueOf(data[2]));
+        Calendar now = Calendar.getInstance();
+        int day = now.get(Calendar.DAY_OF_MONTH) - birthday.get(Calendar.DAY_OF_MONTH);
+        int month = now.get(Calendar.MONTH) + 1 - birthday.get(Calendar.MONTH);
+        int year = now.get(Calendar.YEAR) - birthday.get(Calendar.YEAR);
+        //按照减法原理，先day相减，不够向month借；然后month相减，不够向year借；最后year相减。
+        if (day < 0) {
+            month -= 1;
+            //得到上一个月，用来得到上个月的天数。
+            now.add(Calendar.MONTH, -1);
+            day = day + now.getActualMaximum(Calendar.DAY_OF_MONTH);
+        }
+        if(day == 0){
+            return year*12+month;
+        }else {
+            return year*12+month+1;
+        }
+    }
 }
