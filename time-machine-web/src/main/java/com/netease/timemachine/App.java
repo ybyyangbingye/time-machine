2d39623053b71a13c81e9e20afd4c4662fb9e4d8
bingye <wb-yby488771@alibaba-inc.com>
2019-05-24 20:22:42 +0800

    @feat 更新future

--- time-machine-web/src/main/java/com/netease/timemachine/App.java
+++ time-machine-web/src/main/java/com/netease/timemachine/App.java
@@ -1,5 +1,6 @@
 package com.netease.timemachine;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -13,6 +14,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  **/
 @SpringBootApplication(scanBasePackages = {"com.netease.timemachine"},exclude = DataSourceAutoConfiguration.class)
 @EnableScheduling
+@MapperScan("com.netease.timemachine")
 @EnableCaching
 public class App {
     public static void main(String[] args) {
